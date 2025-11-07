1832badb710a6939938d9bb72013c6ec581843bf
actualchrisw <actualchrisw@gmail.com>
2025-11-06 18:17:43 -0700

    created database tables

--- db/schema.sql
+++ db/schema.sql
@@ -1,1 +1,16 @@
--- TODO
+DROP TABLE orders_products;
+DROP TABLE orders;
+DROP TABLE products;
+DROP TABLE users;
+
+-- Create Tables
+CREATE TABLE users():
+
+
+CREATE TABLE orders();
+
+CREATE TABLE products();
+
+CREATE TABLE orders_products(); -- JUNCTION TABLE
+
+
