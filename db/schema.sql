a1a040a1dd484c0cc1d58fea1684fba7fc40bf32
actualchrisw <actualchrisw@gmail.com>
2025-11-06 19:00:51 -0700

    added databse seeding

--- db/schema.sql
+++ db/schema.sql
@@ -4,13 +4,29 @@ DROP TABLE products;
 DROP TABLE users;
 
 -- Create Tables
-CREATE TABLE users():
+CREATE TABLE users(
+    id SERIAL PRIMARY KEY,
+    username TEXT UNIQUE NOT NULL,
+    password TEXT NOT NULL
+);
 
+CREATE TABLE orders(
+    id SERIAL PRIMARY KEY,
+    date DATE NOT NULL,
+    note TEXT,
+    user_id INT NOT NULL REFERENCES users(id) ON DELETE CASCADE
+);
 
-CREATE TABLE orders();
+CREATE TABLE products(
+    id SERIAL PRIMARY KEY,
+    title TEXT NOT NULL,
+    description TEXT NOT NULL,
+    price NUMERIC(2) NOT NULL 
+);
 
-CREATE TABLE products();
-
-CREATE TABLE orders_products(); -- JUNCTION TABLE
-
-
+CREATE TABLE orders_products( -- JUNCTION TABLE
+    order_id INT NOT NULL REFERENCES orders(id) ON DELETE CASCADE,
+    product_id INT NOT NULL REFERENCES products(id) ON DELETE CASCADE,
+    quantity INT NOT NULL,
+    PRIMARY KEY (order_id, product_id)
+);
--- db/seed.js
+++ db/seed.js
@@ -6,5 +6,31 @@ await db.end();
 console.log("🌱 Database seeded.");
 
 async function seed() {
-  // TODO
-}
+  const products = [
+    ["Acoustic Guitar", "Spruce top", 99],
+    ["Electric Guitar", "Dual humbucker", 89],
+    ["Drum Sticks", "Hickory, pair", 11],
+    ["Keyboard Stand", "Adjustable", 25],
+    ["Microphone", "Vocal", 55],
+    ["Guitar Strings", "Nickel wound", 18],
+    ["Guitar Amp", "2x12 30W", 79],
+    ["Tuner", "Chromatic", 33],
+    ["Guitar Strap", "Multicolor", 13],
+    ["Music Stand", "Folding", 29],
+  ];
+
+  try {
+    await db.query("BEGIN");
+  }
+};
+
+const userRes = await db.query(
+  `
+  INSERT INTO users (username, password
+  VALUES ($1, $2)
+  RETURNING id;
+  `,
+  ["testuser", "testpassword"]
+);
+
+const userID = userRes.rows[0].id;
