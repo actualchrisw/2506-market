10771a745cdbcab67d7e1f06f5aef7ea55940e2b
Chris Warman <actualchrisw@gmail.com>
2025-11-23 14:15:11 -0700

    Add files via upload

--- db/client.js
+++ db/client.js
@@ -1,3 +1,21 @@
 import pg from "pg";
-const db = new pg.Client(process.env.DATABASE_URL);
-export default db;
+import { loadSchema, seedDatabase } from "./setup.js";
+
+const connectionString = process.env.DATABASE_URL || "postgres://localhost/market";
+const client = new pg.Client(connectionString);
+
+let initialized;
+const originalConnect = client.connect.bind(client);
+
+client.connect = async (...args) => {
+  await originalConnect(...args);
+  if (!initialized && process.env.NODE_ENV === "test") {
+    initialized = (async () => {
+      await loadSchema(client);
+      await seedDatabase(client);
+    })();
+    await initialized;
+  }
+};
+
+export default client;
